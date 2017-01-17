e40e67935dcc395346dd0c6864eda7020be9f710
JG-Nicholson <j.g.nicholson@qmul.ac.uk>
2017-01-17 15:56:07 +0000

    updated to work with your data
    1) takes as input  i) a list of txt files with: ref_seqID, p-value, FPKM & gene names ii) gene reference list with: ref_seqID,  gene names & gene symbols
    2) output (saved as files)
       i) tables -  raw data matrix
                      - lists of DE genes for each comparison
                      - a table of the the limma model
      ii) Graphs - exploratory data analysis on full gene set (PCA & HC)
                       - volcano plots for each comparison
                       - heat map of the DE genes
                       - exploratory data analysis on DE gene set (PCA & HC)

--- DNTA_analysis+visulaizations.R
+++ DNTA_analysis+visulaizations.R
@@ -1,234 +1,228 @@
-#### RNA-seq analysis using limma & edgeR
-#### NB this analysis starts from raw counts & library sizes - therfore it will need to be adapted
-#### to enable the input of our FPKM data - however DGE analysis and data visulaization will remian
-#### approcimately constant
+#### RNA-seq analysis of FPKM data using limma & edgeR
 
 rm (list =ls())  # clear the workkspace
 
-#### installing the required packages
+#### installing the required packages if needed
 #install.packages('limma')
 #install.packages('edgeR')
 #install.packages('dendextend')
 #install.packages('gplots')
+#install.packages('calibrate')
 
 ### loading the required packages
 library(limma)
 library(edgeR)
 library(dendextend)
 library(gplots)
-library(RColorBrewer)
+library(calibrate)
 
-### downloading & unzipping the input files 
-### Each of these text files contains the raw gene-level counts for a given sample.
-url <- "http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE63310&format=file"
+################ loading in the data ####################
 
-utils::download.file(url, destfile="GSE63310_RAW.tar", mode="wb") 
-utils::untar("GSE63310_RAW.tar", exdir = ".")
-files <- c("GSM1545535_10_6_5_11.txt", "GSM1545536_9_6_5_11.txt", "GSM1545538_purep53.txt",
-           "GSM1545539_JMS8-2.txt", "GSM1545540_JMS8-3.txt", "GSM1545541_JMS8-4.txt",
-           "GSM1545542_JMS8-5.txt", "GSM1545544_JMS9-P7c.txt", "GSM1545545_JMS9-P8c.txt")
-for(i in paste(files, ".gz", sep=""))
-R.utils::gunzip(i, overwrite=TRUE)
-
-files <- c("GSM1545535_10_6_5_11.txt", "GSM1545536_9_6_5_11.txt", 
-           "GSM1545538_purep53.txt", "GSM1545539_JMS8-2.txt", 
-           "GSM1545540_JMS8-3.txt", "GSM1545541_JMS8-4.txt", 
-           "GSM1545542_JMS8-5.txt", "GSM1545544_JMS9-P7c.txt", 
-           "GSM1545545_JMS9-P8c.txt")
-
-read.delim(files[1], nrow=5) # this just checks what is the files
+files <- c('10s0r1.uniq.txt', '11s0r2.uniq.txt', '12s0r3.uniq.txt',
+           '13s8r1.uniq.txt','14s8r2.uniq.txt', '15s8r3.uniq.txt',
+            '16s24r1.uniq.txt', '17s24r2.uniq.txt', '18s24r3.uniq.txt')
+# read.delim(files[x], nrow=y) # use to check file content
 
 ### readDGE function creates a DGEList-object containing 1) sample info  & 2) expression matrix 
-### NB in this sample data it is Entrez ID, GeneLength & raw count
-
 x <- readDGE(files, columns=c(1,3))
-class(x)
-dim(x)
+# class(x) # inspecting the data
+# dim(x) # inspecting the data
+# x # inspecting the data
 
-### adding additional information, ie experimetal group, batch number/date etc
-samplenames <- substring(colnames(x), 12, nchar(colnames(x)))
-samplenames
-colnames(x) <- samplenames
-group <- as.factor(c("LP", "ML", "Basal", "Basal", "ML", "LP", 
-                     "Basal", "ML", "LP"))
+### adding additional experimetal group information
+samplenames <- colnames(x)
+group <- as.factor(c("Uninfected", "Uninfected", "Uninfected", "Hendra_8hrs", "Hendra_8hrs", "Hendra_8hrs", 
+                     "Hendra_24hrs", "Hendra_24hrs", "Hendra_24hrs"))
 x$samples$group <- group
-x$samples$group
+#x$samples$group
 
-### here we are finding the gene names from their ENTREZID compared against the installed
-### mouse annotations
-geneid <- rownames(x)
-genes <- select(Mus.musculus, keys=geneid, columns=c("SYMBOL", "TXCHROM"), 
-                keytype="ENTREZID")
+### reading in the gene reference table
+### col1= ref_seqID, col2=long gene name, col3 = gene symbol
+genes <- read.table(file="functions3.txt", fill = TRUE)
+colnames(genes) <- c('ref_seqID', 'gene', 'symbol')
+rownames(genes) <- genes$ref_seqID
+# head(genes) # inspecting the data
 
-head(genes)
-genes <- genes[!duplicated(genes$ENTREZID),] # removing duplicates
-x$genes <- genes        # adding the gene information to the DGElist object
+########### writing out a data matrix for the user to download #############
 
-x  # check what the final DGElist object contains
-
-
-#### Data pre-processing
-# now converting reads to CPM (in our case we will just start with FPKM)
-cpm <- cpm(x)
-lcpm <- cpm(x, log=TRUE)
-
-table(rowSums(x$counts==0)==9)   # this shows us the genes with no expression in any samples
-                                 # we shouldn't have any of these
+df <- data.frame(x$counts)
+GeneID <- genes[rownames(x),3]
+df <- cbind(df, GeneID)
+write.table(df, file = "results/tables/data_matrix.txt")
 
 
-# Genes must be expressed in at least one group (or in at least three samples across ...
-# ... the entire experiment) to be kept for downstream analysis.
+#################### data filtering #######################
 
-dim(x) # before normalization (27179 rows)
-keep.exprs <- rowSums(cpm>1)>=3
+### changing our FPKM values to Log2(fpkm) and setting log2(0) to 0
+x$counts <- log2(x$counts)
+x$counts[is.infinite(x$counts)] <- 0
+# head(x$counts) # inspecting the data
+
+table(rowSums(x$counts==0)==9)  # this shows us the genes with no expression in any samples
+# Genes must be expressed in at least one group or in >3 samples to be kept for downstream analysis.
+# dim(x) # before filtering (18907)
+lfpkm <- x$counts
+keep.exprs <- rowSums(lfpkm>1)>=3
 x <- x[keep.exprs,, keep.lib.sizes=FALSE]
-dim(x) # after normalization (14165 rows)
-
-### graphically demonstrating the filtering using density plots
-### - this would be a nice feature to return to our user 
-par(mfrow = c(1, 2))
-nsamples <- ncol(x)
-col <- brewer.pal(nsamples, "Paired")
-par(mfrow=c(1,2))
-plot(density(lcpm[,1]), col=col[1], lwd=2, ylim=c(0,0.21), las=2, 
-     main="", xlab="")
-title(main="A. Raw data", xlab="Log-cpm")
-abline(v=0, lty=3)
-for (i in 2:nsamples){
-  den <- density(lcpm[,i])
-  lines(den$x, den$y, col=col[i], lwd=2)
-}
-legend("topright", samplenames, text.col=col, bty="n", cex=0.75)
-
-lcpm <- cpm(x, log=TRUE)
-plot(density(lcpm[,1]), col=col[1], lwd=2, ylim=c(0,0.21), las=2, 
-     main="", xlab="")
-title(main="B. Filtered data", xlab="Log-cpm")
-abline(v=0, lty=3)
-for (i in 2:nsamples){
-  den <- density(lcpm[,i])
-  lines(den$x, den$y, col=col[i], lwd=2)
-}
-legend("topright", samplenames, text.col=col, bty="n", cex=0.75)
+# dim(x) # after filtering (14021)
 
 
-### normalizing the gene expression distributions
-### it is assumed that all samples should have a similar range and distribution of expression values
-### this can be altered by batch effects etc
-### Normalisation ensures expression distributions are similar across the entire experiment.
+################## Unsupervised exploratory data analysis ##################
+##########PCA & HCA on total data set (ie BEFORE DGE-analysis)##############
 
-par(mfrow = c(1, 1))
-x <- calcNormFactors(x, method = "TMM")
-x$samples$norm.factors
-boxplot(lcpm, las=2, col=col, main="")    
-title(main="Normalised expression distributions",ylab="Log-cpm")
-
-
-### Unsupervised exploratory data analysis. PCA & HCA on total data set (ie BEFORE DGE-analysis)
 ### PCA plot
-lcpm <- t(cpm(x, log=TRUE))
-Xpca <-prcomp(lcpm, scale= TRUE)
+pdf('results/graphs/PCA_all_data.pdf')
+lfpkm <- t(x$counts)
+Xpca <-prcomp(lfpkm)
 Xscores   <- Xpca$x
 plot(Xscores, xlab="PC1", ylab="PC2", pch=19, cex=1,
      cex.lab=0.7, cex.axis = 0.7, col=group,
      main="Principal components analysis")
 text(Xscores, labels=group, cex= 1, pos=1)
+dev.off()
+
 
 ### HCA dendrogram
-hc <-hclust(dist(lcpm))
+pdf('results/graphs/HC_all_data.pdf')
+hc <-hclust(dist(lfpkm))
 dend <-as.dendrogram(hc)
-colorCodes <-c(Basal = "black", LP = "red", ML = "green")
+colorCodes <-c(Uninfected = "black", Hendra_8hrs  = "red", Hendra_24hrs = "green")
 labels(dend) <- group[order.dendrogram(dend)]
 labels_colors(dend) <- colorCodes[group][order.dendrogram(dend)]
 plot(dend)
+dev.off()
 
-### differntial gen expression analysis
+
+############ Differntial Gene Expression Analysis ###############
+
+# building the design matrix (ie setting sample groups)
 design <- model.matrix(~0+group)
 colnames(design) <- gsub("group", "", colnames(design))
 design
 
+# building the contrast matrix 
 contr.matrix <- makeContrasts(
-  BasalvsLP = Basal-LP, 
-  BasalvsML = Basal - ML, 
-  LPvsML = LP - ML, 
+  Uninf_vs_H_8hrs = Uninfected - Hendra_8hrs, 
+  Uninf_vs_H_24hrs = Uninfected - Hendra_24hrs, 
+  H_8hrs_vs_H_24hrs = Hendra_8hrs - Hendra_24hrs, 
   levels = colnames(design))
 contr.matrix
 
-###### Here I use Voom (which starts from raw counts) - we will skip this bit
-v <- voom(x, design)
-v
 ##### we will start here swapping v for our fpkm table
-vfit <- lmFit(v, design)
-vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
-efit <- eBayes(vfit)    # we will use trend=TRUE here
-
-
-
+fit <- lmFit(x$counts, design)
+fit <- contrasts.fit(fit, contrasts=contr.matrix)
+efit <- eBayes(fit, trend=TRUE)    # we will use trend=TRUE here
 summary(decideTests(efit))    # this shows if genes are up/downegulated or unchanged
-tfit <- treat(vfit, lfc=1)    # The treat method is used to calculate p-values from empirical Bayes 
-dt <- decideTests(tfit)       # moderated t-statistics with a minimum log-FC requirement.
-summary(dt)
+dt <- decideTests(efit)   
 
-de.common <- which(dt[,1]!=0 & dt[,2]!=0)  # this identifies those gene that are different between both basal & LP and basal & MP
-length(de.common)
+#### this selects those genes that are differentially expressed from each comparrision
+DE_UvsT8 <- which(dt[,1]!=0)
+DE_UvsT24 <- which(dt[,2]!=0)
+DE_T8vsT24 <- which(dt[,3]!=0)
+DE_all <- c(which(dt[,1]!=0), which(dt[,2]!=0) , which(dt[,3]!=0))
+DE_all <- unique(DE_all)
 
-head(tfit$genes$SYMBOL[de.common], n=20)
-write((tfit$genes$SYMBOL[de.common]), file="de_common.txt") # this writes out a list of the diffectially expressed genes 
-write.fit(tfit, dt, file="results.txt")   # this saves a tsv txt file of the DGE reults
+#### this looks up their gene names
+de_UvsT8 <- genes[DE_UvsT8,]
+de_UvsT24 <- genes[DE_UvsT24,]
+de_T8vsT24 <- genes[DE_T8vsT24,]
+de_all <- genes[DE_all,]
+
+### this writes out the list of DE genes for each comparision
+write.table(de_UvsT8, file="results/tables/de_UvsT8.txt", row.names=FALSE)
+write.table(de_UvsT24, file="results/tables/de_UvsT24.txt", row.names=FALSE)
+write.table(de_T8vsT24, file="results/tables/de_T8vsT24.txt", row.names=FALSE)
+write.table(de_all, file="results/tables/de_all.txt", row.names=FALSE)
+
+### this writes out a table describing the limma derived model
+write.fit(efit, dt, file="results/tables/de_limma_model.txt")
+
+
+######################### Volcano Plots ######################
+# 1) make the plot of logFC vs -log10(P.Value)
+# 2) highlight those genes with adj.P.Val < 0.05 & |logFC|>2) in red
+# 3) label significant genes using the genes lookup table
+
 
 ### Examining individual DE genes - topTreat displays they in descending order of significance
-basal.vs.lp <- topTreat(tfit, coef=1, n=Inf)
-basal.vs.ml <- topTreat(tfit, coef=2, n=Inf)
-head(basal.vs.lp)
-head(basal.vs.ml)
-
-### volcano plots
-par(mfrow = c(1, 2))
-with(basal.vs.lp, plot(basal.vs.lp$logFC, -log10(basal.vs.lp$P.Value), pch=20, 
-          main="Basal vs LP", xlim=c(-10,10), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
-with(basal.vs.ml, plot(basal.vs.ml$logFC, -log10(basal.vs.lp$P.Value), pch=20, 
-          main="Basal vs ml", xlim=c(-10,10), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
+Uninf_vs_H_8hrs <- topTreat(efit, coef=1, n=Inf)
+Uninf_vs_H_24hrs <- topTreat(efit, coef=2, n=Inf)
+H_8hrs_vs_H_24hrs <- topTreat(efit, coef=3, n=Inf)
+# head(Uninf_vs_H_8hrs)    # - inspecting the data 
+# head(Uninf_vs_H_24hrs)   # - inspecting the data 
+# head(H_8hrs_vs_H_24hrs)  # - inspecting the data 
 
 
-### now ploting a heatmap
+#### Uninfected vs Hendra_8hrs
+pdf('results/graphs/Uninf_vs_H_8hrs_Volcano.pdf')
+with(Uninf_vs_H_8hrs, plot(logFC, -log10(P.Value), pch=20, main="Uninfected vs Hendra_8hrs",
+                                xlim=c(-8,8), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
+text(-6.5,-log10(0.05),col="red","P.Value = 0.05", pos = 3, cex=0.8)
+with(subset(Uninf_vs_H_8hrs, adj.P.Val  < 0.05 & abs(logFC)>2), points(logFC, -log10(P.Value),
+                                pch=20, , cex =0.5,col="red"))
+with(subset(Uninf_vs_H_8hrs, adj.P.Val  < 0.05 & abs(logFC)>2), textxy(logFC, -log10(P.Value), 
+                                labs=genes[rownames(subset(Uninf_vs_H_8hrs,
+                                adj.P.Val  < 0.05 & abs(logFC)>2)),3], cex=.8))
+dev.off()
 
-library(gplots)
-topgenes <- c(basal.vs.lp$ENTREZID[1:40], basal.vs.ml$ENTREZID[1:40])
-#length(topgenes)
-topgenes <- unique(topgenes)
-#length(topgenes)
+#### Uninfected vs Hendra_24hrs
+pdf('results/graphs/Uninf_vs_H_24hrs_Volcano.pdf')
+with(Uninf_vs_H_24hrs, plot(logFC, -log10(P.Value), pch=20, main="Uninfected vs Hendra_24hrs",
+                                xlim=c(-8,8), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
+text(-6.5,-log10(0.05),col="red","P.Value = 0.05", pos = 3, cex=0.8)
+with(subset(Uninf_vs_H_24hrs, adj.P.Val  < 0.05 & abs(logFC)>2), points(logFC, -log10(P.Value),
+                                pch=20, , cex =0.5,col="red"))
+with(subset(Uninf_vs_H_24hrs, adj.P.Val  < 0.05 & abs(logFC)>2), textxy(logFC, -log10(P.Value), 
+                                labs=genes[rownames(subset(Uninf_vs_H_24hrs,
+                                adj.P.Val  < 0.05 & abs(logFC)>2)),3], cex=.4))
+dev.off()
 
-i <- which(x$genes$ENTREZID %in% topgenes)
-par(mfrow = c(1, 1))
-heatmap.2(v$E[i,], col=redgreen(30), scale="row", 
-          labRow=v$genes$SYMBOL[i], labCol=group,
+#### Hendra_8hrs vs Hendra_24hrs
+pdf('results/graphs/H_8hrs_vs_H_24hrs_Volcano.pdf')
+with(H_8hrs_vs_H_24hrs, plot(logFC, -log10(P.Value), pch=20, main="Hendra_8hrs vs Hendra_24hrs",
+                                     xlim=c(-8,8), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
+text(-6.5,-log10(0.05),col="red","P.Value = 0.05", pos = 3, cex=0.8)
+with(subset(H_8hrs_vs_H_24hrs, adj.P.Val  < 0.05 & abs(logFC)>2), points(logFC, -log10(P.Value),
+                                     pch=20, , cex =0.5,col="red"))
+with(subset(H_8hrs_vs_H_24hrs, adj.P.Val  < 0.05 & abs(logFC)>2), textxy(logFC, -log10(P.Value), 
+                                     labs=genes[rownames(subset(H_8hrs_vs_H_24hrs,
+                                     adj.P.Val  < 0.05 & abs(logFC)>2)),3], cex=.4))
+dev.off()
+
+############ Plotting a Heatmap of DE_Genes ###########
+
+# the x$count data matrix and genes lookup table are subset by the DE_all genes list
+pdf('results/graphs/Topgenes_Heatmap.pdf')
+heatmap.2(x$counts[DE_all[1:60],], col=redgreen(30), scale="row", 
+          labRow=genes[DE_all[1:60],3], labCol=group,
           key=T, keysize=1, density.info="none",
-          trace= "none", cexCol=0.9, dendrogram="column",
-          margin=c(2,14))
+          trace= "none", cexCol=0.9, cexRow = 0.5, 
+          margin=c(6,9))
+dev.off()
 
+################## Unsupervised exploratory data analysis ##################
+############### PCA & HCA on genes identified by DGE-analysis ##############
 
-### Unsupervised exploratory data analysis. PCA & HCA on genes identified by DGE-analysis
 ### PCA plot
-lcpm <- t(cpm(x, log=TRUE))
-lcpm <- lcpm[,i]
-Xpca <-prcomp(lcpm, scale= TRUE)
+pdf('results/graphs/PCA_topgenes.pdf')
+lfpkm <- t(x$counts[DE_all,])
+Xpca <-prcomp(lfpkm, scale= TRUE)
 Xscores   <- Xpca$x
 plot(Xscores, xlab="PC1", ylab="PC2", pch=19, cex=1,
      cex.lab=0.7, cex.axis = 0.7, col=group,
      main="Principal components analysis")
 text(Xscores, labels=group, cex= 1, pos=1)
-
-
-
+dev.off()
 
 ### HCA dendrogram
-hc <-hclust(dist(lcpm[,i]))
+pdf('results/graphs/HC_topgenes.pdf')
+hc <-hclust(dist(lfpkm))
 dend <-as.dendrogram(hc)
 colorCodes <-c(Basal = "black", LP = "red", ML = "green")
 labels(dend) <- group[order.dendrogram(dend)]
 labels_colors(dend) <- colorCodes[group][order.dendrogram(dend)]
 plot(dend)
-
+dev.off()
 
 sessionInfo()
 
