a6dfe64e57fe4c7a4241b131041319a88edd962a
JG-Nicholson <j.g.nicholson@qmul.ac.uk>
2017-01-24 10:12:14 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+
+
+#!/usr/bin/python
+#export FLASK_DEBUG=1
+
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+import module
+import subprocess
+import os
+
+
+app = Flask(__name__)
+#app = Flask(static_folder='/Users/james/Desktop/FlaskApp/static')
+
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# This route will show a form to perform an AJAX request
+@app.route("/")
+def main():
+    return render_template('index2.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded files
+    uploaded_files = request.files.getlist("file[]")
+    filenames = []
+    for file in uploaded_files:
+        # Check if the file is one of the allowed types/extensions
+        if file and allowed_file(file.filename):
+            # Make the filename safe, remove unsupported chars
+            filename = secure_filename(file.filename)
+            # Move the file form the temporal folder to the upload
+            # folder we setup
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # Save the filename into a list, we'll use it later
+            filenames.append(filename)
+            # Redirect the user to the uploaded_file route, which
+            # will basicaly show on the browser the uploaded file
+            # Load an html page with a link to each uploaded file
+    return render_template('upload.html', filenames=filenames)
+
+
+ #
+@app.route('/results', methods=['POST'])
+def analyze():
+    groups = request.form.getlist('experimental group')
+    path = './uploads'
+    raw_files = os.listdir(path)[1:len(os.listdir(path))]
+    module.data_prep(raw_files)
+    blast_results = module.run_blast(raw_files)
+    module.format_blast(blast_results)
+    module.genelist()
+    module.R_analysis(groups)
+    return render_template('results.html')
+    
+
+# This route is expecting a parameter containing the name of a file. Then it will locate that 
+# file on the upload directory and show it on the browser
+@app.route('/uploads/<filename>')
+def uploaded_file(filename=None):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == "__main__":
+    app.run()
