0b45ff363e658586c7c095e7d84bf2ed28f9a192
Yusef Badi <y.e.badi@se11.qmul.ac.uk>
2017-02-05 19:31:32 +0000

    comments in line with issue #16 on master branch

--- scripts/apocrita/call_blast.sh
+++ scripts/apocrita/call_blast.sh
@@ -1,4 +1,8 @@
 #### NB this script is hosted and executed on apocrita
+
+#$ -pe smp 4       # Request 4 CPU cores (please add this to the script executed in apocrita so we can get 4 cores)
+#set the number GB per core to 1 please
+
 #### it has been included here as a reference only
 
 export SGE_ROOT="/opt/sge"
