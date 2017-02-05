65d4889c4844910e67ee65166f0d11d6373f59a1
Yusef Badi <y.e.badi@se11.qmul.ac.uk>
2017-02-05 20:39:12 +0000

    Update module.py

--- module.py
+++ module.py
@@ -12,85 +12,49 @@ def data_prep(raw_files):
         os.system(cmd)
         print (raw_file, "done")
 
-def run_blast(raw_files):
-    print(" now running blast")
-    blast_results=[]
-    for file in raw_files:
-        result = ('blast_', file)
-        result = ''.join(result)
-        blast_results.append(result)
-
-    for (file, blast_result) in zip(raw_files, blast_results):
-        cmd="blastn -query ", file, " -db reference.fasta.txt -evalue 0.001 -num_threads 4 -outfmt \"6 qseqid sseqid evalue stitle\" -max_target_seqs 1 -out", result
-        cmd=''.join(cmd)
-        os.system(cmd)
-        print (blast_result, "complete")
-    return blast_results
-
-def format_blast(blast_results):
-    print("fomatting data output")
-    
-    ints_1=[]
-    for blast_result in blast_results:
-        int_1= ('int_1_', blast_result)
-        int_1= ''.join(int_1)
-        ints_1.append(int_1)  
-    
-    ints_2=[]
-    for blast_result in blast_results:
-        int_2= ('int_2_', blast_result)
-        int_2= ''.join(int_2)
-        ints_2.append(int_2)
-
-    ## this code extracts the FPKM value
-    for (blast_result, int_1)  in zip(blast_results, ints_1):
-        cmd = "awk -F ';' '/^asmbl/{print $2; next}{print}' < temp/", blast_result, " > temp/", int_1
-        cmd = ''.join(cmd)
-        os.system(cmd)
-
-    ## this code reformats the tables
-    for (int_1, int_2)  in zip( ints_1, ints_2):
-        cmd = "awk \'{out= $2 \"\\t\" $3 \"\\t\" $1 \"\\t\"; for(i=7;i<=NF;i++){out=out " "$i \"_\" }; print out}' < temp/" , int_1, " > temp/", int_2
-        cmd = ''.join(cmd)
-        print (int_2, "done")
-        os.system(cmd)
-
-    ## this code removes duplicates & saves output in blast/ for use by R
-    # this needs to be optimised so it isn't randomly deleting redundant duplicates - may be valuable information
-    print("removing duplicates")
-
-    for (int_2,blast_result) in zip(ints_2, blast_results):
-        cmd= "sort -u -k1,1 temp/", int_2, " > blast/", blast_result
-        cmd= ''.join(cmd)
-        os.system(cmd)
-        print(blast_result, "done")
-
-    ### this clears the temp folder of intermediates
-    cmd= 'rm temp/int*'      
-    os.system(cmd)
-    cmd= 'rm temp/1*'      
+def apocrita_upload(user_id):
+    files = "/uploads/" + user_id + "/*.txt"
+    cmd = "scp ./uploads/*.txt bt16003@login.hpc.qmul.ac.uk:/data/scratch/bt16003/group_project/raw"
+    print cmd 
     os.system(cmd)
 
-def genelist():
-    cmd= "cat blast/b* | sort -u -k1,1 | awk '{out= $1 \"\\t\" $4; print out}' | awk -F '[()]' '{out= $1$2$3$4$5 \"\\t\" $2 \"\\t\" $4 ; print out}' | awk '{print $1 \"\\t\" $2 \"\\t\" $NF}' > blast/1_gene_list.txt"
+def apocrita_blast():
+    cmd = "ssh bt16003@login.hpc.qmul.ac.uk /data/home/bt16003/call_blast.sh"
+    print cmd 
     os.system(cmd)
 
+def apocrita_download(user_id):
+    remote_folder = "/data/scratch/bt16003/group_project/results/" + user_id + "/*"
+    local_folder = "./blast/" + user_id + '/'
+    cmd = "scp bt16003@login.hpc.qmul.ac.uk:" + remote_folder + " " + local_folder 
+    os.system(cmd)
+    print "blast results downloaded"
 
-def R_analysis(groups):
-    path='./blast'
+def pdf_combine():
+    cmd= "gs -q -sPAPERSIZE=a4 -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=static/results/graphs/all_graphs.pdf static/results/graphs/*.pdf"
+    os.system(cmd)
+
+def R_analysis(groups, user_id):
+    path='./blast/' + user_id
     x = os.listdir(path)[1:len(os.listdir(path))]
     files=[]
     for file in x:
-        file= ('blast/',file)
-        file= ''.join(file)
+        file= os.path.join('blast', user_id, file)
         files.append(file)
-
+    print x
     command = 'Rscript'
     path2script = 'scripts/analysis.R'
     args = files + groups
-
     cmd = [command, path2script] + args
     subprocess.call(cmd, universal_newlines=True)
     return "analysis complete"
 
-
+def clean_up(user_id):
+    cmd = 'rm blast/' + user_id + '/*' 
+    os.system(cmd)
+    cmd = 'rm uploads/' + user_id + '/*' 
+    os.system(cmd)
+    cmd = "ssh bt16003@login.hpc.qmul.ac.uk 'rm /data/scratch/bt16003/group_project/raw/" + user_id + "/*' " 
+    os.system(cmd)
+    cmd = "ssh bt16003@login.hpc.qmul.ac.uk 'rm /data/scratch/bt16003/group_project/results/" + user_id + "/*' "
+    os.system(cmd)
