9a067bb89e9146f613841e4dd6e89ae2979ffe97
Yusef Badi <y.e.badi@se11.qmul.ac.uk>
2017-02-05 20:52:07 +0000

    updated all user Ids

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 #!/usr/bin/python
 #export FLASK_DEBUG=1
 
@@ -10,8 +8,6 @@ import subprocess
 import os
 import uuid
 
-
-
 app = Flask(__name__)
 #app = Flask(static_folder='/Users/james/Desktop/FlaskApp/static')
 
@@ -20,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 
 # These are the extension that we are accepting to be uploaded
-app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'fasta'])
 
 # For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
@@ -30,15 +26,15 @@ def allowed_file(filename):
 # This route will show a form to perform an AJAX request
 @app.route("/")
 def main():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 @app.route('/choose', methods=['POST'])
 def choose():
     return render_template('choose_uploads.html')
 
-@app.route('/loader', methods=['POST'])
-def loader():
-    return render_template('loader.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -53,11 +49,11 @@ def upload():
     #now we're making a unique user ID and saving it to the session (cookie)
     session['user'] = uuid.uuid1()
     user_folder = os.path.join(app.config['UPLOAD_FOLDER'], session['user'])
-    
-    #if path doesn't exist then make it
-    if not os.path.exists(user_folder):
-        os.makedirs(user_folder)
 
+    #if path doesn't exist then make it
+    if not in os.path.exists(user_folder):
+        os.makedirs(user_folder)
+    
     for file in uploaded_files:
         # Check if the file is one of the allowed types/extensions
         if file and allowed_file(file.filename):
@@ -71,24 +67,21 @@ def upload():
             filenames.append(filename)
             # Redirect the user to the uploaded_file route, which
             # will basicaly show on the browser the uploaded file
-            # Load an html page with a link to each uploaded file
+            # Load an html page with a link to each uploaded file     
     return render_template('upload.html', filenames=filenames, groups=groups)
-
 
 
 @app.route('/results', methods=['POST'])
 def analyze():
-    # groups = request.form.getlist('experimental group')
-    # path = './uploads'
-    # raw_files = os.listdir(path)[1:len(os.listdir(path))]
-    # module.data_prep(raw_files)
-    # blast_results = module.run_blast(raw_files)
-    # module.format_blast(blast_results)
-    # module.genelist()
-    # module.R_analysis(groups)
-    return render_template('results2.html')
+    module.apocrita_upload(session['user'])
+    module.apocrita_blast()
+    module.apocrita_download(session['user'])
+    groups = request.form.getlist('experimental group', session['user'])
+    module.R_analysis(groups, session['user'])
+    module.pdf_combine(session['user'])
+    module.clean_up(session['user'])
+    return render_template('results.html')
     
-
 # This route is expecting a parameter containing the name of a file. Then it will locate that 
 # file on the upload directory and show it on the browser
 @app.route('/uploads/<filename>')
@@ -99,5 +92,3 @@ def uploaded_file(filename=None):
 if __name__ == "__main__":
     app.secret_key = "t3tmiU+u7VXna4M98i+G3IXqjZ5ZSmrviQgbiyy5Tcw6kdwu/uZ0gw+0pLbzRsBINFjc6FjRd9QTsbByFSmS9w=="
     app.run()
-
-    
