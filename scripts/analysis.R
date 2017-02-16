5af87e5e9d3c8adf5a9824e0bd130a1cc75545ae
James Nicholson <j.g.nicholson@qmul.ac.uk>
2017-02-16 15:08:52 +0000

    Add files via upload

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,19 +1,20 @@
-# Could have raw_input from user for number of columns and use this object throughout so it overcomes the 3 groups barrier?
-#alternatively if this proves to be too difficult; to meet the deadline we can have a set of hardcoded R scripts for various group numbers
-#and call the appropriate script according to the number of groups the end-user picks in FLASK
-
 ##### RNA-seq analysis of FPKM data using limma & edgeR
 
 rm (list =ls())  # clear the workkspace
 
-#### installing the required packages if needed
+
+
+######### loading the required packages ###############
+
+
+
+# installing the required packages if needed
 #install.packages('limma')
 #install.packages('edgeR')
 #install.packages('dendextend')
 #install.packages('gplots')
 #install.packages('calibrate')
 
-### loading the required packages
 library(limma)
 library(edgeR)
 library(dendextend)
@@ -21,37 +22,39 @@ library(gplots)
 library(calibrate)
 
 
-# Reading in the variables set by python
-myArgs <- commandArgs(trailingOnly = TRUE)
-myArgs
-# useful for working
-# myArgs <- c("blast/1_gene_list.txt", "blast/blast_10s0r1.fasta.txt", "blast/blast_11s0r2.fasta.txt",
-#              "blast/blast_12s0r3.fasta.txt", "blast/blast_13s8r1.fasta.txt", "blast/blast_14s8r2.fasta.txt" ,
-#              "blast/blast_15s8r3.fasta.txt", "blast/blast_16s24r1.fasta.txt", "blast/blast_17s24r2.fasta.txt" ,
-#              "blast/blast_18s24r3.fasta.txt", "Uninfected", "Uninfected", "Uninfected", "Hendra_8hrs",
-#              "Hendra_8hrs", "Hendra_8hrs", "Hendra_24hrs", "Hendra_24hrs", "Hendra_24hrs")
-# # myArgs
-genes  <- read.table(file=myArgs[1], fill = TRUE)
-myArgs <- myArgs[2:length(myArgs)]
 
-x <- length(myArgs)
-files  <- c(myArgs[1:(x/2)])
+########## Reading in the variables set by python#######
+
+
+myArgs <- commandArgs(trailingOnly = TRUE) 
+myArgs       
+
+genes  <- read.table(file=myArgs[1])   # reads in 1_gene_list.txt used to look up gene names by reference ID
+myArgs <- myArgs[2:length(myArgs)]     # remove 1_gene_list.txt from the mrArgs list
+
+x <- length(myArgs)                          # the first half of the myArgs represent the sample file names              
+files  <- c(myArgs[1:(x/2)])                 # the second half of the myArgs represent their experimental groups
 group  <- as.factor(c(myArgs[((x/2)+1):x]))
 
-# extracting variable names for downstream use
+group_number<- length(unique(group))   # can be 2, 3 or 4 used downstream to dictate analysis options
 
-group_IDs <- unique(group)
+group_IDs <- unique(group)             # extracting the name of each group
+group_IDs
 group_1 <- toString(group_IDs[1])
 group_2 <- toString(group_IDs[2])
 group_3 <- toString(group_IDs[3])
+group_4 <- toString(group_IDs[4])
+
+
 
 ################ loading in the data ####################
+
+
 
 ### readDGE function creates a DGEList-object containing 1) sample info  & 2) expression matrix 
 x <- readDGE(files, columns=c(1,3))
 # class(x) # inspecting the data
 # dim(x) # inspecting the data
-# inspecting the data
 
 ### adding additional experimetal group information
 samplenames <- colnames(x)
@@ -62,34 +65,44 @@ colnames(genes) <- c('ref_seqID', 'gene', 'symbol')
 rownames(genes) <- genes$ref_seqID
 #head(genes) # inspecting the data
 
-########### writing out a data matrix for the user to download #############
+
+
+## writing out a data matrix for the user to download ##
 
 df <- data.frame(x$counts)
 GeneID <- genes[rownames(x),3]
-df <- cbind(df, GeneID)
+df <- cbind(GeneID, df)
 write.table(df, file = "static/results/tables/1.data_matrix.tsv",  quote=FALSE, sep='\t', row.names=FALSE)
 
+
+
 #################### data filtering #######################
+
+
 
 ### changing our FPKM values to Log2(fpkm) and setting log2(0) to 0
 x$counts <- log2(x$counts)
 x$counts[is.infinite(x$counts)] <- 0
-# head(x$counts) # inspecting the data
+head(x$counts) # inspecting the data
 
-#table(rowSums(x$counts==0)==9)  # this shows us the genes with no expression in any samples
+
 # Genes must be expressed in at least one group or in >3 samples to be kept for downstream analysis.
-# dim(x) # before filtering (18907)
+#dim(x) # before filtering (~18907)
 lfpkm <- x$counts
 keep.exprs <- rowSums(lfpkm>1)>=3
 x <- x[keep.exprs,, keep.lib.sizes=FALSE]
-# dim(x) # after filtering (14021)
+#dim(x) # after filtering (~14021)
 
 
-################## Unsupervised exploratory data analysis ##################
-##########PCA & HCA on total data set (ie BEFORE DGE-analysis)##############
+
+########## Unsupervised exploratory data analysis #############
+####PCA & HCA on total data set (ie BEFORE DGE-analysis)#######
+
+
 
 ### PCA plot
-png('static/results/graphs/1.PCA_all_data.png')
+
+pdf('static/results/graphs/1.PCA_all_data.pdf')
 lfpkm <- t(x$counts)
 Xpca <-prcomp(lfpkm)
 Xscores   <- Xpca$x
@@ -99,9 +112,8 @@ plot(Xscores, xlab="PC1", ylab="PC2", pch=19, cex=1,
 text(Xscores, labels=group, cex= 1, pos=1)
 dev.off()
 
-
 ### HCA dendrogram
-png('static/results/graphs/2.HC_all_data.png')
+pdf('static/results/graphs/2.HC_all_data.pdf')
 hc <-hclust(dist(lfpkm))
 dend <-as.dendrogram(hc)
 colorCodes <-c(group_1 = "black", group_2  = "red", group_3 = "green")
@@ -111,73 +123,96 @@ plot(dend)
 dev.off()
 
 
+
 ############ Differntial Gene Expression Analysis ###############
+
+
 
 # building the design matrix (ie setting sample groups)
 design <- model.matrix(~0+group)
 colnames(design) <- gsub("group", "", colnames(design))
 design
 
-# building the contrast matrix 
-contr_1 <- paste(c(group_1,"-",group_2), sep = "", collapse = " ")
-contr_2 <- paste(c(group_1,"-",group_3), sep = "", collapse = " ")
-contr_3 <- paste(c(group_2,"-",group_3), sep = "", collapse = " ")
 
-contr.matrix <- makeContrasts(
-  contr_1,
-  contr_2, 
-  contr_3, 
-  levels = colnames(design))
-contr.matrix
+### The following code builds a contrast matrix with 1, 3 or 6
+### comaprisions depending on the number of experimental groups (group_number)
 
-# delete if everything works
-#contr.matrix <- makeContrasts(
-#  Uninf_vs_H_8hrs = Uninfected - Hendra_8hrs, 
-#  Uninf_vs_H_24hrs = Uninfected - Hendra_24hrs, 
-#  H_8hrs_vs_H_24hrs = Hendra_8hrs - Hendra_24hrs, 
-#  levels = colnames(design))
-#contr.matrix
+if (group_number ==2){
+  contr_1 <- paste(c(group_1,"-",group_2), sep = "", collapse = " ") 
+  
+  contr.matrix <- makeContrasts(
+    contr_1,
+    levels = colnames(design))
+  contr.matrix
+  
+} else if (group_number == 3){
+  contr_1 <- paste(c(group_1,"-",group_2), sep = "", collapse = " ")
+  contr_2 <- paste(c(group_1,"-",group_3), sep = "", collapse = " ")
+  contr_3 <- paste(c(group_2,"-",group_3), sep = "", collapse = " ")
+  
+  contr.matrix <- makeContrasts(
+    contr_1, contr_2, contr_3, 
+    levels = colnames(design))
+  contr.matrix
+  
+} else if (group_number == 4){
+  contr_1 <- paste(c(group_1,"-",group_2), sep = "", collapse = " ")
+  contr_2 <- paste(c(group_1,"-",group_3), sep = "", collapse = " ")
+  contr_3 <- paste(c(group_1,"-",group_4), sep = "", collapse = " ")
+  contr_4 <- paste(c(group_2,"-",group_3), sep = "", collapse = " ")
+  contr_5 <- paste(c(group_2,"-",group_4), sep = "", collapse = " ")
+  contr_6 <- paste(c(group_3,"-",group_4), sep = "", collapse = " ")
+  
+  contr.matrix <- makeContrasts(
+    contr_1, contr_2, contr_3, contr_4, contr_5, contr_6,
+    levels = colnames(design))
+  contr.matrix
+}
 
-##### we will start here swapping v for our fpkm table
+##### Using limma to perform DGE analysis according to the ######
+############## set design and contrast matrix ###################
+
 fit <- lmFit(x$counts, design)
 fit <- contrasts.fit(fit, contrasts=contr.matrix)
-efit <- eBayes(fit, trend=TRUE)    # we will use trend=TRUE here
-efit
+efit <- eBayes(fit, trend=TRUE)    
+
 #summary(decideTests(efit))    # this shows if genes are up/downegulated or unchanged
 dt <- decideTests(efit)   
 
-#### this selects those genes that are differentially expressed from each comparrision
-#DE_UvsT8 <- rownames(as.matrix(which(dt[,1]!=0)))
-#DE_UvsT24 <- rownames(as.matrix(which(dt[,2]!=0)))
-#DE_T8vsT24 <- rownames(as.matrix(which(dt[,3]!=0)))
-#DE_all <- c(DE_UvsT8,DE_UvsT24,DE_T8vsT24)
-#DE_all <- unique(DE_all)
-#length(DE_all)
+### The following code pulls out the refID of those gense determined to
+### be differentially expressed by the limma model
 
-DE_grp1_vs_grp2 <- rownames(as.matrix(which(dt[,1]!=0)))
-DE_grp1_vs_grp3 <- rownames(as.matrix(which(dt[,2]!=0)))
-DE_grp2_vs_grp3 <- rownames(as.matrix(which(dt[,3]!=0)))
-DE_all <- c(DE_grp1_vs_grp2 , DE_grp1_vs_grp3, DE_grp2_vs_grp3)
-DE_all <- unique(DE_all)
+if (group_number ==2){
+  DE_grp1_vs_grp2 <- rownames(as.matrix(which(dt[,1]!=0)))
+  DE_all <- DE_grp1_vs_grp2
+  
+} else if (group_number == 3){
+  DE_grp1_vs_grp2 <- rownames(as.matrix(which(dt[,1]!=0)))
+  DE_grp1_vs_grp3 <- rownames(as.matrix(which(dt[,2]!=0)))
+  DE_grp2_vs_grp3 <- rownames(as.matrix(which(dt[,3]!=0)))
+  DE_all <- c(DE_grp1_vs_grp2 , DE_grp1_vs_grp3, DE_grp2_vs_grp3)
+  DE_all <- unique(DE_all)
+  
+} else if (group_number == 4){
+  DE_grp1_vs_grp2 <- rownames(as.matrix(which(dt[,1]!=0)))
+  DE_grp1_vs_grp3 <- rownames(as.matrix(which(dt[,2]!=0)))
+  DE_grp1_vs_grp4 <- rownames(as.matrix(which(dt[,3]!=0)))
+  DE_grp2_vs_grp3 <- rownames(as.matrix(which(dt[,4]!=0)))
+  DE_grp2_vs_grp4 <- rownames(as.matrix(which(dt[,5]!=0)))
+  DE_grp3_vs_grp4 <- rownames(as.matrix(which(dt[,6]!=0)))
+  DE_all <- c(DE_grp1_vs_grp2 , DE_grp1_vs_grp3, DE_grp1_vs_grp4, DE_grp2_vs_grp3, DE_grp2_vs_grp4,DE_grp3_vs_grp4 )
+  DE_all <- unique(DE_all)
+}
 
+de_all <- genes[DE_all,] # this looks up their gene names from the refID
 
-#### this looks up their gene names
-de_grp1_vs_grp2 <- genes[DE_grp1_vs_grp2 ,]
-de_grp1_vs_grp3 <- genes[DE_grp1_vs_grp3 ,]
-de_grp2_vs_grp3 <- genes[DE_grp2_vs_grp3 ,]
-de_all <- genes[DE_all,]
 
 ### here we are forming a merged de_all + evalue table for visualisations
 e_values <- efit$p.value[DE_all,]
 merged <- merge(de_all,e_values, by.x=1, by.y=0)
-
-
-### this writes out the list of DE genes for each comparision
-write.table(de_grp1_vs_grp2, file="static/results/tables/2.de_grp1_vs_grp2.tsv", quote=FALSE, sep='\t', row.names=FALSE)
-write.table(de_grp1_vs_grp3, file="static/results/tables/3.de_grp1_vs_grp3.tsv", quote=FALSE, sep='\t', row.names=FALSE)
-write.table(de_grp2_vs_grp3, file="static/results/tables/4.de_grp2_vs_grp3.tsv", quote=FALSE, sep='\t', row.names=FALSE)
-write.table(de_all, file="static/results/tables/5.de_all.tsv", quote=FALSE, sep='\t', row.names=FALSE)
 write.table(merged, file="static/results/tables/6.de_all_with_evalues.tsv", quote=FALSE, sep='\t', row.names=FALSE)
+
+
 
 ######################### Volcano Plots ######################
 # 1) make the plot of logFC vs -log10(P.Value)
@@ -185,69 +220,96 @@ write.table(merged, file="static/results/tables/6.de_all_with_evalues.tsv", quot
 # 3) label significant genes using the genes lookup table
 
 
-### Examining individual DE genes - topTreat displays they in descending order of significance
-topT_grp1_vs_grp2 <- topTreat(efit, coef=1, n=Inf)
-topT_grp1_vs_grp3 <- topTreat(efit, coef=2, n=Inf)
-topT_grp2_vs_grp3 <- topTreat(efit, coef=3, n=Inf)
+### Examining individual DE genes - topTreat displays them in descending order of significance
+### here we evaluate the different coeffiects of the topTreat on efit depending on group_number
+
+if (group_number ==2){
+  topT_grp1_vs_grp2 <- topTreat(efit, coef=1, n=Inf)
+  topT_list <- list(topT_grp1_vs_grp2)
+  
+} else if (group_number == 3){
+  topT_grp1_vs_grp2 <- topTreat(efit, coef=1, n=Inf)  
+  topT_grp1_vs_grp3 <- topTreat(efit, coef=2, n=Inf)
+  topT_grp2_vs_grp3 <- topTreat(efit, coef=3, n=Inf)
+  topT_list <- list(topT_grp1_vs_grp2, topT_grp1_vs_grp3, topT_grp2_vs_grp3)
+  
+} else if (group_number == 4){
+  topT_grp1_vs_grp2 <- topTreat(efit, coef=1, n=Inf)  
+  topT_grp1_vs_grp3 <- topTreat(efit, coef=2, n=Inf)
+  topT_grp1_vs_grp4 <- topTreat(efit, coef=3, n=Inf)
+  topT_grp2_vs_grp3 <- topTreat(efit, coef=4, n=Inf)  
+  topT_grp2_vs_grp4 <- topTreat(efit, coef=5, n=Inf)
+  topT_grp3_vs_grp4 <- topTreat(efit, coef=6, n=Inf)
+  topT_list <- list(topT_grp1_vs_grp2, topT_grp1_vs_grp3, topT_grp1_vs_grp4, topT_grp2_vs_grp3, topT_grp2_vs_grp4, topT_grp3_vs_grp4)
+
+}
+
+# Generatng the comparison names for Volcano plot titles
+if (group_number ==2){
+  comp_1 <- paste(c(group_1,"vs",group_2), sep = "", collapse = " ")
+  comp_list <- list(comp_1)
+  
+} else if (group_number == 3){
+  comp_1 <- paste(c(group_1,"vs",group_2), sep = "", collapse = " ")
+  comp_2 <- paste(c(group_1,"vs",group_3), sep = "", collapse = " ")
+  comp_3 <- paste(c(group_2,"vs",group_3), sep = "", collapse = " ")
+  comp_list <- list(comp_1, comp_2, comp_3)
+
+} else if (group_number == 4){
+  comp_1 <- paste(c(group_1,"vs",group_2), sep = "", collapse = " ")
+  comp_2 <- paste(c(group_1,"vs",group_3), sep = "", collapse = " ")
+  comp_3 <- paste(c(group_1,"vs",group_4), sep = "", collapse = " ")
+  comp_4 <- paste(c(group_2,"vs",group_3), sep = "", collapse = " ")
+  comp_5 <- paste(c(group_2,"vs",group_4), sep = "", collapse = " ")
+  comp_6 <- paste(c(group_3,"vs",group_4), sep = "", collapse = " ")
+  comp_list <- list(comp_1, comp_2, comp_3, comp_4, comp_5, comp_6)
+}
 
 
-# comparison names for graph titles
-comp_1 <- paste(c(group_1,"vs",group_2), sep = "", collapse = " ")
-comp_2 <- paste(c(group_1,"vs",group_3), sep = "", collapse = " ")
-comp_3 <- paste(c(group_2,"vs",group_3), sep = "", collapse = " ")
+graph_names=list()
+for (i in 1:length(topT_list)){
+  graph_names <- c(graph_names, (paste(c('static/results/graphs/Volcano_',i,'.pdf'), sep = "", collapse = "")))
+}
 
-#### Uninfected vs Hendra_8hrs
-png('static/results/graphs/3.grp1_vs_grp2_Volcano.png')
-with(topT_grp1_vs_grp2, plot(logFC, -log10(P.Value), pch=20, main=comp_1,
-                                xlim=c(-8,8), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
-text(-6.5,-log10(0.05),col="red","P.Value = 0.05", pos = 3, cex=0.8)
-with(subset(topT_grp1_vs_grp2, adj.P.Val  < 0.05 & abs(logFC)>2), points(logFC, -log10(P.Value),
-                                pch=20, , cex =0.5,col="red"))
-with(subset(topT_grp1_vs_grp2, adj.P.Val  < 0.05 & abs(logFC)>2), textxy(logFC, -log10(P.Value), 
-                                labs=genes[rownames(subset(topT_grp1_vs_grp2,
-                                adj.P.Val  < 0.05 & abs(logFC)>2)),3], cex=.8))
-dev.off()
+#For loop used to plot a titled Volcano plot for each experimental comparison
 
-#### Uninfected vs Hendra_24hrs
-png('static/results/graphs/4.grp1_vs_grp3_Volcano.png')
-with(topT_grp1_vs_grp3, plot(logFC, -log10(P.Value), pch=20, main=comp_2,
-                                xlim=c(-8,8), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
-text(-6.5,-log10(0.05),col="red","P.Value = 0.05", pos = 3, cex=0.8)
-with(subset(topT_grp1_vs_grp3, adj.P.Val  < 0.05 & abs(logFC)>2), points(logFC, -log10(P.Value),
-                                pch=20, , cex =0.5,col="red"))
-with(subset(topT_grp1_vs_grp3, adj.P.Val  < 0.05 & abs(logFC)>2), textxy(logFC, -log10(P.Value), 
-                                labs=genes[rownames(subset(topT_grp1_vs_grp3,
-                                adj.P.Val  < 0.05 & abs(logFC)>2)),3], cex=.4))
-dev.off()
+for (i in 1:length(topT_list)){
+  pdf(graph_names[[i]])
+  with(topT_list[[i]], plot(logFC, -log10(P.Value), pch=20, main=comp_list[[i]],
+                            xlim=c(-8,8), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
+  text(-6.5,-log10(0.05),col="red","P.Value = 0.05", pos = 3, cex=0.8)
+  with(subset(topT_list[[i]], adj.P.Val  < 0.05 & abs(logFC)>2), points(logFC, -log10(P.Value),
+                            pch=20, , cex =0.5,col="red"))
+  with(subset(topT_list[[i]], adj.P.Val  < 0.05 & abs(logFC)>2), textxy(logFC, -log10(P.Value), 
+                            labs=genes[rownames(subset(topT_list[[i]],
+                            adj.P.Val  < 0.05 & abs(logFC)>2)),3], cex=.4))
+  dev.off()
+}
 
-#### Hendra_8hrs vs Hendra_24hrs
-png('static/results/graphs/5.grp2_vs_grp3_Volcano.png')
-with(topT_grp2_vs_grp3 , plot(logFC, -log10(P.Value), pch=20, main=comp_3,
-                                     xlim=c(-8,8), cex =0.25, abline(h=-log10(0.05),col="red",lty="44")))
-text(-6.5,-log10(0.05),col="red","P.Value = 0.05", pos = 3, cex=0.8)
-with(subset(topT_grp2_vs_grp3 , adj.P.Val  < 0.05 & abs(logFC)>2), points(logFC, -log10(P.Value),
-                                     pch=20, , cex =0.5,col="red"))
-with(subset(topT_grp2_vs_grp3 , adj.P.Val  < 0.05 & abs(logFC)>2), textxy(logFC, -log10(P.Value), 
-                                     labs=genes[rownames(subset(topT_grp2_vs_grp3 ,
-                                     adj.P.Val  < 0.05 & abs(logFC)>2)),3], cex=.4))
-dev.off()
+
 
 ############ Plotting a Heatmap of DE_Genes ###########
 
+
+
 # the x$count data matrix and genes lookup table are subset by the DE_all genes list
-png('static/results/graphs/6.Topgenes_Heatmap.png')
-heatmap.2(x$counts[DE_all[1:60],], col=redgreen(30), scale="row", 
-          labRow=genes[DE_all[1:60],3], labCol=group,
+pdf('static/results/graphs/6.Topgenes_Heatmap.pdf')
+heatmap.2(x$counts[DE_all[1:100],], col=redgreen(30), scale="row", 
+          labRow=genes[DE_all[1:100],3], labCol=group,
           key=T, keysize=1, density.info="none",
           trace= "none", cexCol=0.9, cexRow = 0.5, 
           margin=c(6,9))
 dev.off()
 
+
+
 ################## Unsupervised exploratory data analysis ##################
 ############### PCA & HCA on genes identified by DGE-analysis ##############
 
+
+
 ### PCA plot
-png('static/results/graphs/7.PCA_topgenes.png')
+pdf('static/results/graphs/7.PCA_topgenes.pdf')
 lfpkm <- t(x$counts[DE_all,])
 Xpca <-prcomp(lfpkm, scale= TRUE)
 Xscores   <- Xpca$x
@@ -257,8 +319,10 @@ plot(Xscores, xlab="PC1", ylab="PC2", pch=19, cex=1,
 text(Xscores, labels=group, cex= 1, pos=1)
 dev.off()
 
+
+
 ### HCA dendrogram
-png('static/results/graphs/8.HC_topgenes.png')
+pdf('static/results/graphs/8.HC_topgenes.pdf')
 hc <-hclust(dist(lfpkm))
 dend <-as.dendrogram(hc)
 colorCodes <-c(group_1 = "black", group_2  = "red", group_3 = "green")
@@ -267,7 +331,7 @@ labels_colors(dend) <- colorCodes[group][order.dendrogram(dend)]
 plot(dend)
 dev.off()
 
-rm (list =ls()) 
+rm (list =ls())              # clearing the workspace
 z= "analysis complete :) "
 z
 #sessionInfo()
