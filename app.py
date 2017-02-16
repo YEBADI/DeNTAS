955cc9f429dcdaf1595e6637e02f0070ed07f755
James Nicholson <j.g.nicholson@qmul.ac.uk>
2017-02-16 15:09:50 +0000

    final DeNTAS scripts

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
 #export FLASK_DEBUG=1
 
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
 import module
 import subprocess
 import os
-import uuid
+import numpy
+
 
 app = Flask(__name__)
-#app = Flask(static_folder='/Users/james/Desktop/FlaskApp/static')
 
 # This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 
-
-# These are the extension that we are accepting to be uploaded
+# These are the extensions that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'fasta'])
 
 # For a given file, return whether it's an allowed type or not
@@ -23,64 +22,64 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-# This route will show a form to perform an AJAX request
+# This is the main app route and will return the index page with information
+# about the project
 @app.route("/")
 def main():
     return render_template('index.html')
 
-@app.route('/choose', methods=['POST'])
-def choose():
-    return render_template('choose_uploads.html')
-
+# This is app route and will return the about page with further information
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# This app route returns the first analysis page in which the user inputs the names
+# of their exp.groups and chooses their raw data to upload
+@app.route('/choose', methods=['POST'])
+def choose():
+    module.clear_previous_results()       # clear previous results before a new run
+    return render_template('choose_uploads.html')
+
+
+# This app route returns the second analysis page in which the user selects their model
+# organism, and chooses the exp. group for each sample from a drop down box
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Get the name of the uploaded files
-    uploaded_files = request.files.getlist("file[]")
-    filenames = []
+    uploaded_files = request.files.getlist("file[]") # Get the names of the uploaded files
+    
     g1 = request.form.get('Group 1')
     g2 = request.form.get('Group 2')
     g3 = request.form.get('Group 3')
     g4 = request.form.get('Group 4')
-    groups = [g1, g2 , g3 , g4]
-    #now we're making a unique user ID and saving it to the session (cookie)
-    session['user'] = uuid.uuid1()
-    user_folder = os.path.join(app.config['UPLOAD_FOLDER'], session['user'])
-
-    #if path doesn't exist then make it
-    if not in os.path.exists(user_folder):
-        os.makedirs(user_folder)
-    
-    for file in uploaded_files:
-        # Check if the file is one of the allowed types/extensions
-        if file and allowed_file(file.filename):
-            # Make the filename safe, remove unsupported chars
-            filename = secure_filename(file.filename)
-            # Move the file form the temporal folder to the upload
-            # folder we setup
-            #now each user folder is unique and files are saved respectively there
-            file.save(os.path.join(user_folder, filename))
-            # Save the filename into a list, we'll use it later
-            filenames.append(filename)
-            # Redirect the user to the uploaded_file route, which
-            # will basicaly show on the browser the uploaded file
-            # Load an html page with a link to each uploaded file     
+    groups = [g1, g2 , g3 , g4]                 #create a list of exp groups
+    filenames = []                              
+    for file in uploaded_files:                  
+        if file and allowed_file(file.filename):       # is file extension allowed?     
+            filename = secure_filename(file.filename)  # Make safe (remove unsupported chars) 
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # Save the file in uploads
+            filenames.append(filename)                 # add filename to list         
+    # Load html page with i) links to uploaded files ii) dropdown boxes populated by groups     
     return render_template('upload.html', filenames=filenames, groups=groups)
 
 
-@app.route('/results', methods=['POST'])
+@app.route('/results', methods=['POST'])         #see module.py for more detailed function description
 def analyze():
-    module.apocrita_upload(session['user'])
-    module.apocrita_blast()
-    module.apocrita_download(session['user'])
-    groups = request.form.getlist('experimental group', session['user'])
-    module.R_analysis(groups, session['user'])
-    module.pdf_combine(session['user'])
-    module.clean_up(session['user'])
-    return render_template('results.html')
+    module.apocrita_upload()                     # copies files from local (uploads) to remote (raw) folder
+    organism = request.form.get('organism')      #from dropdown box on pervious page
+    module.apocrita_blast(organism)              #runs organism specific call_blast.sh over ssh
+    module.apocrita_download()                   # copies files from remote (results) to local (blast) folder
+    groups = request.form.getlist('experimental group')  # each sample assigned a group
+    module.R_analysis(groups)
+    module.pdf_combine()                         # R output collated 
+    module.clean_up()                            # removes temporary files folders
+    ngroups = len(numpy.unique(groups))          
+    # different results pages (to accomodate different numbers of graphs) 
+    if ngroups==2:
+        return render_template('results2.html')
+    elif ngroups==3:
+        return render_template('results3.html')
+    elif ngroups==4:
+        return render_template('results4.html')
     
 # This route is expecting a parameter containing the name of a file. Then it will locate that 
 # file on the upload directory and show it on the browser
@@ -90,5 +89,4 @@ def uploaded_file(filename=None):
                                filename)
 
 if __name__ == "__main__":
-    app.secret_key = "t3tmiU+u7VXna4M98i+G3IXqjZ5ZSmrviQgbiyy5Tcw6kdwu/uZ0gw+0pLbzRsBINFjc6FjRd9QTsbByFSmS9w=="
-    app.run()
+    app.run(debug=True)      #turn debugger & reloading feature on for development
--- module.py
+++ module.py
@@ -2,59 +2,84 @@
 import subprocess
 import os
 
-
+### This function can be used to add a ">" to the fasta titles in a mulitfasta if incorrectly formatted
 def data_prep(raw_files):
     print("preparing data")
-
     for raw_file in raw_files:
         cmd = "awk '/^asmbl/{print \">\" $1; next}{print}' < uploads/", raw_file, "> temp/", raw_file  # adding the >
         cmd = ''.join(cmd)
         os.system(cmd)
         print (raw_file, "done")
 
-def apocrita_upload(user_id):
-    files = "/uploads/" + user_id + "/*.txt"
-    cmd = "scp ./uploads/*.txt bt16003@login.hpc.qmul.ac.uk:/data/scratch/bt16003/group_project/raw"
+### The following functions enable the use of Apocrita to run Blast on the users uploaded files remotely
+
+### Copy the contents from DeNTAS' uploads folder to Apocrita's raw folder
+def apocrita_upload():
+    cmd = "scp ./uploads/* bt16003@login.hpc.qmul.ac.uk:/data/scratch/bt16003/group_project/raw"
     print cmd 
     os.system(cmd)
 
-def apocrita_blast():
-    cmd = "ssh bt16003@login.hpc.qmul.ac.uk /data/home/bt16003/call_blast.sh"
-    print cmd 
-    os.system(cmd)
+### The user-selcted organism determins which set of blast scripts are called over ssh
+### <organism>_call_blast.sh runs the qsub script <organism>_run _blast.sh which then 
+### submits the <organism>_blast.py job to the Apocrita quing system
+def apocrita_blast(organism):
+    if organism == 'P. alecto':
+        cmd = "ssh bt16003@login.hpc.qmul.ac.uk /data/home/bt16003/P_alecto_call_blast.sh"
+        print cmd 
+        os.system(cmd)
+    elif organism == 'H. sapiens':
+        cmd = "ssh bt16003@login.hpc.qmul.ac.uk /data/home/bt16003/H_sapiens_call_blast.sh"
+        print cmd 
+        os.system(cmd)
+    elif organism == 'M. musculus':
+        cmd = "ssh bt16003@login.hpc.qmul.ac.uk /data/home/bt16003/M_musculus_call_blast.sh"
+        print cmd 
+        os.system(cmd)
 
-def apocrita_download(user_id):
-    remote_folder = "/data/scratch/bt16003/group_project/results/" + user_id + "/*"
-    local_folder = "./blast/" + user_id + '/'
-    cmd = "scp bt16003@login.hpc.qmul.ac.uk:" + remote_folder + " " + local_folder 
+### Copy the contents from Apocrita's results folder to DeNTAS' blast folder
+def apocrita_download():
+    cmd = "scp bt16003@login.hpc.qmul.ac.uk:/data/scratch/bt16003/group_project/results/* ./blast/"
     os.system(cmd)
     print "blast results downloaded"
 
+### Use ghostscript (a 3rd part suite of pdf manipulation software) to join all
+### of the data anlaysis pdf output
 def pdf_combine():
     cmd= "gs -q -sPAPERSIZE=a4 -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=static/results/graphs/all_graphs.pdf static/results/graphs/*.pdf"
     os.system(cmd)
 
-def R_analysis(groups, user_id):
-    path='./blast/' + user_id
-    x = os.listdir(path)[1:len(os.listdir(path))]
+def R_analysis(groups):
+    x = os.listdir('./blast')[1:len(os.listdir('./blast'))]    # subset to ignore the hidden .DStore file
     files=[]
-    for file in x:
-        file= os.path.join('blast', user_id, file)
+    for file in x:              # complete path of filenames read into R
+        file= ('blast/',file)
+        file= ''.join(file)
         files.append(file)
-    print x
+    print x                     # check correct filenames in terminal 
     command = 'Rscript'
     path2script = 'scripts/analysis.R'
     args = files + groups
-    cmd = [command, path2script] + args
-    subprocess.call(cmd, universal_newlines=True)
+    cmd = [command, path2script] + args           # constructing the subprocess command 
+    subprocess.call(cmd, universal_newlines=True) # begining analysis and parsing in required Args  
     return "analysis complete"
 
-def clean_up(user_id):
-    cmd = 'rm blast/' + user_id + '/*' 
+### emptpy the apps local uploads & blast folders and Apocrita's remote raw & results folders
+def clean_up():
+    cmd = 'rm blast/*' 
     os.system(cmd)
-    cmd = 'rm uploads/' + user_id + '/*' 
+    cmd = 'rm uploads/*' 
     os.system(cmd)
-    cmd = "ssh bt16003@login.hpc.qmul.ac.uk 'rm /data/scratch/bt16003/group_project/raw/" + user_id + "/*' " 
+    cmd = "ssh bt16003@login.hpc.qmul.ac.uk 'rm /data/scratch/bt16003/group_project/raw/*' "
     os.system(cmd)
-    cmd = "ssh bt16003@login.hpc.qmul.ac.uk 'rm /data/scratch/bt16003/group_project/results/" + user_id + "/*' "
+    cmd = "ssh bt16003@login.hpc.qmul.ac.uk 'rm /data/scratch/bt16003/group_project/temp/*' "
     os.system(cmd)
+    cmd = "ssh bt16003@login.hpc.qmul.ac.uk 'rm /data/scratch/bt16003/group_project/results/*' "
+    os.system(cmd)
+
+### emptpy the local results folders filled by previous runs
+def clear_previous_results():
+    cmd = 'rm static/results/graphs/*' 
+    os.system(cmd)
+    cmd = 'rm static/results/tables/*'
+    os.system(cmd)
+    
