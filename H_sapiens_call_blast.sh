145cdbb38584c513c0025d8c2d999a7b0c2bd623
James Nicholson <j.g.nicholson@qmul.ac.uk>
2017-02-16 15:06:34 +0000

    Delete H_sapiens_call_blast.sh

--- H_sapiens_call_blast.sh
+++
@@ -1,7 +0,0 @@
-# <username> must be changed to reflect your apocrita username eg bt16003
-# this script will only work if you have set up a secure key access to apocrita
-
-export SGE_ROOT="/opt/sge"
-/opt/sge/bin/lx-amd64/qsub -sync y /data/home/<username>/H_sapiens_run_blast.sh
-
-
